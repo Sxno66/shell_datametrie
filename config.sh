ae256ab4bbdbcf5938d3df2214e67989f742cf77
Megaghosty <153129021+Megaghosty@users.noreply.github.com>
2024-11-26 14:17:17 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,4 +4,5 @@
 serveurs=("www.ac-rennes.fr" "www.perplexity.ai" "www.google.com"  "www.twitch.tv") # Liste des serveurs
 intervalle=30 # temps en secondes
 log_file="log_performance.csv"
+mail="destroyer.ghost55@gmail.com"
 max_response_time=200 # temps de réponse maximum en ms pour un test fructueux
