0e6d45e91218e0018566c86eddf5c5a92c6f6f57
LeoPinzano <lucatherogue@gmail.com>
2024-11-26 14:03:36 +0100

    Add files via upload
    Ajout du script de ping / requete et de son fichier de config

---
+++ config.sh
@@ -0,0 +1,7 @@
+# config.sh
+
+# Configuration parameters
+serveurs=("www.ac-rennes.fr" "www.perplexity.ai" "www.google.com"  "www.twitch.tv") # Liste des serveurs
+intervalle=30 # temps en secondes
+log_file="log_performance.csv"
+max_response_time=200 # temps de réponse maximum en ms pour un test fructueux
